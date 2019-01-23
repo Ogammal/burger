5256cbaa14e67e9980634b0aebe0c200f9931cd3
ogammal <gammal93@mac.com>
2019-01-23 17:23:13 -0500

    add routes to burger controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,22 @@ var express = require('express');
 var burger = require('../models/burger.js');
 var router = express.Router();
 
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(err, data) {
+        if (err) throw err;
+        res.render('index', { burgers: data });
+    });
+})
+
+router.post("/api/:burgerName", function(req, res) {
+    console.log(req.body);
+    var burgerName = req.body.burgerName;
+    console.log("burger name is: " + burgerName);
+    burger.insertOne(burgerName, function(err, data) {
+        if (err) throw err;
+        res.redirect("/")
+    })
+})
+
+module.exports = router;
